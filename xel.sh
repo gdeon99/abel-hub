fb0244061c6c522d17c42ca46f01a37dd21b8ab1
gdeon99 <work@ikhsantrx.web.id>
2026-07-22 11:57:01 +0700

    Update xel.sh

--- xel.sh
+++ xel.sh
@@ -29,11 +29,10 @@ rm -rf SRBMiner-Multi*
 
 echo "3. Mendownload SRBMiner-Multi (Versi Terbaru)..."
 # Menggunakan versi 2.7.5 agar mendukung algoritma xelhashv2
-wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.7.5/SRBMiner-Multi-2-7-5-Linux.tar.xz
-
+wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.4.7/SRBMiner-Multi-2-4-7-Linux.tar.xz
 echo "4. Mengekstrak file (Tunggu beberapa detik)..."
-tar -xf SRBMiner-Multi-2-7-5-Linux.tar.xz
-cd SRBMiner-Multi-2-7-5
+tar -xf SRBMiner-Multi-2-4-7-Linux.tar.xz
+cd SRBMiner-Multi-2-4-7
 
 echo "5. Membunuh saingan (Miner lama yang nyangkut)..."
 pkill -9 -f SRBMiner-MULTI > /dev/null 2>&1 || true
